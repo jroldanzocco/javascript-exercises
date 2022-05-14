a9cb9fa43f1e4f18f0aee0d05451cbaf33fc84fd
Jeremias <jroldanzocco@gmail.com>
2022-05-14 20:56:37 -0300

    Progress on ex number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function (arr, element) {};
+const removeFromArray = function () {
+    let arr = arguments[0];
+    let newArray = []
+    if(arguments.length > 2){
+        for(let i = 1; i < arguments.length; i++){
+            arr.filter(e => e !== arguments[i])
+        
+        }
+}
+    
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
