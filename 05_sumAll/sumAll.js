65e2e7fa044e5da826d0efeead3057f103880db0
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-05-15 06:29:54 -0300

    05 sumAll exercise Done!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+  let numberConfirm = typeof n1 === "number" && typeof n2 === "number";
+  let sum = 0;
+  if (n1 < 0 || n2 < 0 || !numberConfirm) {
+    return "ERROR";
+  } else {
+    for (let i = Math.min(n1, n2); i <= Math.max(n1, n2); i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
