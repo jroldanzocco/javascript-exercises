0994c547822ca12f2af220d6b16c7a165c536a35
Jeremias <jroldanzocco@gmail.com>
2022-05-17 10:40:57 -0300

    07 tempConversion, DONE!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+function round(value, presicion){
+  let multiplicador = Math.pow(10, presicion || 0);
+  return Math.round(value * multiplicador) / multiplicador;
+}
+const ftoc = function(farenheit) {
+  return round(((farenheit - 32) * 5/9),1);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return round(((celsius * 9 / 5) + 32),1);
 };
+
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
