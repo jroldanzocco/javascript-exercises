a580b227244987257f46af90d9f0df703820aeb6
Jeremias <jroldanzocco@gmail.com>
2022-05-17 16:48:37 -0300

    11 getTheTitles DONE

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titleArray = [];
+    for(let i = 0; i < arr.length; i++){
+        let titulos;
+        titulos = arr[i].title;
+        titleArray.push(titulos)
+    }
+    return titleArray;
 };
 
 // Do not edit below this line
