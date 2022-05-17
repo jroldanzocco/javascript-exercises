fa15cc4cdc4f78dbe9245c27b2cac9e201361e1c
Jeremias <jroldanzocco@gmail.com>
2022-05-17 16:24:55 -0300

    09 Palindromes DONE!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reverseStr = str.toLowerCase().replace(/[!.,\s]/g,"").split("").reverse().join("")
+   
+    return str.replace(/[!.,\s]/g,"").toLowerCase() === reverseStr;
 
-};
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
+
