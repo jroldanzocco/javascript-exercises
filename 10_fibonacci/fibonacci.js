4005d1a7507f6cbdbb5b7dc1b7332e57bb13a516
Jeremias <jroldanzocco@gmail.com>
2022-05-17 16:31:09 -0300

    10 Fibonacci - DONE!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let number = Number(n);
+    if(number < 0){
+        return "OOPS"
+    }else if(number === 0){
+        return 0;
+    }else if(number === 1){
+        return 1;
+    }
 
+    else{
+        return fibonacci(number - 1) + fibonacci(number - 2)
+    }
 };
 
 // Do not edit below this line
