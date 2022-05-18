6fdc46a06ef6d7c70af092edde84d627bdc525c9
Jeremias <jroldanzocco@gmail.com>
2022-05-18 02:29:09 -0300

    12 findTheOldest DONE!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    let partial;
+    let oldestAge = -Infinity
+    let index;
+    const ACTUAL_YEAR = 2022;
+    for(let i = 0; i < list.length; i++){
+        if(list[i].yearOfDeath === undefined){
+            list[i].yearOfDeath = ACTUAL_YEAR;
+        }
+        partial = Object.values(list[i]).filter(n => {
+            return typeof(n) ==="number"}).reduce((a, b) => b - a)
+                
+        if(partial > oldestAge){
+            oldestAge = partial
+            index = i
+        }
+    
+    }
+    return list[index];
 };
 
 // Do not edit below this line
